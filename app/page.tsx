466a11a41f062741ea0d9c443a1f971637cab4ec
Vulkanmannen2 <alexander@solutionsskovde.se>
2026-08-25 21:13:53 +0200

    dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import MediaLibrary from "@/app/components/MediaLibrary";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const media = await prisma.media.findMany({
     orderBy: { createdAt: "asc" },
